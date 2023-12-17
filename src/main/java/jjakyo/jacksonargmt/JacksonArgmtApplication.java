9b87a10615d98fb6b6cd1756be309fefae2974f2
jjakyo <jjakyoda@gmail.com>
2023-12-17 17:28:15 +0900

    init commit

---
+++ src/main/java/jjakyo/jacksonargmt/JacksonArgmtApplication.java
@@ -0,0 +1,13 @@
+package jjakyo.jacksonargmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonArgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacksonArgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jjakyo/jacksonargmt/JacksonArgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package jjakyo.jacksonargmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonArgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
